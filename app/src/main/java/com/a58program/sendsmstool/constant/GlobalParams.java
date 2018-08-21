60b5874cfdba006aeb922b083242d51d68661d15
guochunpeng666 <guochunpeng666@163.com>
2018-08-21 10:13:07 +0800

    delete unused resource

--- app/src/main/java/com/a58program/sendsmstool/constant/GlobalParams.java
+++ app/src/main/java/com/a58program/sendsmstool/constant/GlobalParams.java
@@ -1,10 +1,6 @@
 package com.a58program.sendsmstool.constant;
 
 
-import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-
 public class GlobalParams {
 	public static final String FILE_PATH="demo";
 }
